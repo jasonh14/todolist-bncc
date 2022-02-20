9a225da640c7db610b66d3140acbe189762e4484
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-02-20 10:18:37 +0700

    click - btn to remove
    Before u can click the text and it will remove the item now u can only click the min btn (using appendchild so i can select the individual button)

--- main.js
+++ main.js
@@ -2,18 +2,22 @@ addList = (e) => {
   e.preventDefault();
   const inputBox = document.getElementById("text-box");
 
+  let pTag = document.createElement("p");
+  pTag.innerText = inputBox.value;
+
+  let minBtn = document.createElement("BUTTON");
+  minBtn.innerText = "-";
+
   let itemContainer = document.createElement("div");
   itemContainer.className = "item";
-  itemContainer.innerHTML = `
-    <p>${inputBox.value}</p>
-    <button>-</button>
-    `;
+  itemContainer.appendChild(pTag);
+  itemContainer.appendChild(minBtn);
 
   let listContainer = document.getElementById("list-container");
   listContainer.appendChild(itemContainer);
 
   inputBox.value = "";
-  itemContainer.addEventListener("click", () => {
+  minBtn.addEventListener("click", () => {
     listContainer.removeChild(itemContainer);
   });
 };
