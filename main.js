8545aaf96b1928d3b4df8e39ad9387244dc7ed7d
Jason Hartanto <jason.hartanto@binus.ac.id>
2022-02-20 10:24:53 +0700

    Final Version

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ addList = (e) => {
   e.preventDefault();
   const inputBox = document.getElementById("text-box");
 
+  //Create <p> element
   let pTag = document.createElement("p");
   pTag.innerText = inputBox.value;
 
+  //Create button
   let minBtn = document.createElement("BUTTON");
   minBtn.innerText = "-";
 
+  // Create Div as a container to each individual to do list
   let itemContainer = document.createElement("div");
   itemContainer.className = "item";
   itemContainer.appendChild(pTag);
